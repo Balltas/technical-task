2e3260659c6660c5668ff9cb4b16df06ddc4d137
mplycius <marius.plycius@hexito.com>
2023-03-27 23:06:32 +0300

    added one moere test

--- tests/Unit/Service/GeocoderStrategy/WholeStackStrategyTest.php
+++ tests/Unit/Service/GeocoderStrategy/WholeStackStrategyTest.php
@@ -70,4 +70,31 @@ class WholeStackStrategyTest extends TestCase
         $this->assertEquals('12.34', $coordinates->getLat());
         $this->assertEquals('56.78', $coordinates->getLng());
     }
+
+    public function testGetCoordinatesWillReturnNull(): void
+    {
+        $repositoryMock = $this->createMock(ResolvedAddressRepository::class);
+
+        $geocoderMock1 = $this->createMock(GoogleApiGeocoder::class);
+        $geocoderMock2 = $this->createMock(HereApiGeocoder::class);
+
+        $geocoderMock1->expects($this->once())
+            ->method('geocode')
+            ->willReturn(null);
+
+        $geocoderMock2->expects($this->once())
+            ->method('geocode')
+            ->willReturn(null);
+
+        $geocoders = [$geocoderMock1, $geocoderMock2];
+
+        $wholeStackStrategy = new WholeStackStrategy($repositoryMock, $geocoders);
+
+        $address = new Address('LT', 'Kaugonys', 'Gatviu 2', '10001');
+
+        $coordinates = $wholeStackStrategy->getCoordinates($address);
+
+        $this->assertEquals(null, $coordinates);
+
+    }
 }
